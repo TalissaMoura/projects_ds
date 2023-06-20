bf0ad3c429e8ab4c6072a09268904cbf6f205ab5
TalissaMoura <talissa.ayres@gmail.com>
2023-06-20 17:24:50 -0400

    change credentials

--- newsApp/news/__init__.py
+++ newsApp/news/__init__.py
@@ -6,12 +6,17 @@ from urllib.parse import quote_plus
 
 #call python
 app = Flask(__name__)
-app.secret_key = os.environ["FLASK_APP_SECRET_KEY"]
+app_secret_key = open(os.environ["FLASK_APP_SECRET_KEY_FILE"],"r").read()
+app.secret_key = app_secret_key
 app.json_encoder = MongoJSONEncoder
 
+#get user and password info
+user_mdb = open(os.environ["MONGODB_USER_FILE"],"r").read()
+password_mdb = open(os.environ["MONGODB_PASSWORD_FILE"],"r").read()
+
 #connecting to mongo
-user = "user_talissa"
-password = quote_plus(os.environ["MONGODB_PASSWORD"])
+user = quote_plus(user_mdb)
+password = quote_plus(password_mdb)
 database_name = "newsdatabase"
 client = MongoClient(f"mongodb+srv://{user}:{password}@cluster1.rqsrosp.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database("newsdatabase")
